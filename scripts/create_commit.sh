7c1d5b38fa1311e5744bfed798c8d509f328080e
Victor Garcia <vgarciabnz@gmail.com>
2025-04-01 08:19:16 +0200

    Update signed commit

--- scripts/create_commit.sh
+++ scripts/create_commit.sh
@@ -1,8 +1,9 @@
 
-git checkout -b "signed-branch"
+BRANCH="signed-branch"
+git checkout -b $BRANCH
 
 echo foo > bar.txt
 
 git add .
 git commit -S -m "This commit is signed!"
-git push
+git push origin $BRANCH
